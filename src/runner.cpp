87f8475c2fd89a74d3d39cefebf0a4ef01a09cd7
raffifasaro <raffifasaro@gmail.com>
2026-06-21 23:53:59 +0200

    Add cli reader to runner

--- src/runner.cpp
+++ src/runner.cpp
@@ -1,6 +1,22 @@
 #include "runner.h"
 #include <iostream>
+#include <string>
 
 void runInterpreter() {
-    std::cout << "Interpreter running";
+    std::cout << "Interpreter running\n";
+
+    std::string input;
+
+    while(true) {
+
+        std::cout << ">>> ";
+
+        if(!std::getline(std::cin, input) || input == "exit") {
+            break;
+        }
+
+        std::cout << "test " << input;
+
+        std::cout << "\n";
+    }
 }
