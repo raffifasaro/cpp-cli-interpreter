c121e55572dd830d4306b89ddd69c617d812af21
raffifasaro <raffifasaro@gmail.com>
2026-06-28 22:01:04 +0200

    wip

--- modules/parse_math.cpp
+++ modules/parse_math.cpp
@@ -7,7 +7,7 @@ namespace parse_math
 
     struct treeNode
     {
-        std::string_view element;
+        std::string element;
 
         std::unique_ptr<treeNode> left;
         std::unique_ptr<treeNode> right;
@@ -22,6 +22,13 @@ namespace parse_math
     {
         parse_math::treeNode root;
 
+        if (token_input.front().inner == "(" && token_input.back().inner == ")")
+        {
+            token_input.erase(token_input.begin());
+            token_input.pop_back();
+        }
+        
+
         for (size_t i = 0; i < token_input.size(); i++)
         {
             token::Token current = token_input.at(i);
@@ -33,15 +40,35 @@ namespace parse_math
                     i++;
                 }
             }
-            else if (current.inner == "*" || current.inner == "/")
+            else if (current.inner == "+" || current.inner == "-")
             {
                 root.element = current.inner;
                 root.left = std::make_unique<parse_math::treeNode>(parse_math::build_tree({token_input.begin(), token_input.begin() + i}));
                 root.right = std::make_unique<parse_math::treeNode>(parse_math::build_tree({token_input.begin() + i + 1, token_input.end()}));
+                return root;
             }
         }
-        
-        
+
+        for (size_t i = 0; i < token_input.size(); i++)
+        {
+            token::Token current = token_input.at(i);
+
+            if (current.inner == "(")
+            {
+                while (token_input.at(i).inner != ")" && i < token_input.size())
+                {
+                    i++;
+                }
+            }
+            else if (current.inner == "*" || current.inner == "/" || current.inner == "%")
+            {
+                root.element = current.inner;
+                root.left = std::make_unique<parse_math::treeNode>(parse_math::build_tree({token_input.begin(), token_input.begin() + i}));
+                root.right = std::make_unique<parse_math::treeNode>(parse_math::build_tree({token_input.begin() + i + 1, token_input.end()}));
+                return root;
+            }
+        }
+        root.element = token_input.back().inner;
         return root;
     }
 }
