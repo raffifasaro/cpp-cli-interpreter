d8ceef2215cc4fdaa4e6d83a488e36fbe01a8b67
raffifasaro <raffifasaro@gmail.com>
2026-07-11 23:15:36 +0200

    remove redundant iosteram import

--- modules/math/eval_math.cpp
+++ modules/math/eval_math.cpp
@@ -1,8 +1,5 @@
 #include "eval_math.h"
 
-#include <iostream>
-
-
 namespace eval_math
 {
     double evaluate(parser::TreeNode& root_node, std::unordered_map<std::string, std::unique_ptr<variable::Variable>>& variables)
