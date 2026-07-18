30862f1f1b0e6509886ebee55506506f0512ba7b
raffifasaro <raffifasaro@gmail.com>
2026-07-18 23:14:57 +0200

    add simple input error printer

---
+++ src/common/error/error.cpp
@@ -0,0 +1,20 @@
+#include "error.h"
+#include <format>
+
+void print_input_error(std::string err_line, int pos, int length)
+{
+    std::string err_msg(err_line.length(), ' ');
+
+    for (size_t i = pos; i < pos + length; i++)
+    {
+        err_msg[i] = '^';
+    }
+    err_msg.append("\n");
+    err_msg.append("Input error at index " + pos);
+    
+    err_line.append("\n");
+    std::cout << err_line;
+
+    err_msg.append("\n");
+    std::cout << err_msg;
+}
---
+++ src/common/error/error.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+void print_input_error(std::string err_line, int pos, int length);
